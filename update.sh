76e9dd850ab109e5b878111d8e8a038dec112607
Nish Shyam <nshyam668@gmail.com>
2020-05-06 20:16:01 +0100

    Fixed git push in shell script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 /usr/local/bin/jupyter nbconvert --to notebook --execute --inplace trend_analysis.ipynb
 git add .
-git commit -m "test commit"
+git commit -m "daily update jupyter"
 /usr/local/bin/git push origin master
