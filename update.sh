166730870718b2520f06e2b607193f517e6691f1
Nish Shyam <nshyam668@gmail.com>
2020-05-30 19:04:19 +0100

    edited script

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PATH=$PATH:/usr/local/bin/git-credential-osxkeychain
 /usr/local/bin/jupyter nbconvert --to notebook --execute --inplace trend_analysis.ipynb
 git add .
 git commit -m "daily update jupyter"
